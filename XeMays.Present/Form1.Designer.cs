11e889edef56eb423b2aa3acb90f795b2c24274c
Hiệu burando Tô <hieutotoka@gmail.com>
2024-08-06 05:49:51 +0700

    oke

---
+++ XeMays.Present/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace XeMays.Present
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dgv_xemay = new DataGridView();
+            btn_them = new Button();
+            btn_sua = new Button();
+            btn_xoa = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            txt_ten = new TextBox();
+            txt_hang = new TextBox();
+            txt_gia = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)dgv_xemay).BeginInit();
+            SuspendLayout();
+            // 
+            // dgv_xemay
+            // 
+            dgv_xemay.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgv_xemay.Location = new Point(26, 280);
+            dgv_xemay.Name = "dgv_xemay";
+            dgv_xemay.Size = new Size(1029, 267);
+            dgv_xemay.TabIndex = 0;
+            dgv_xemay.CellClick += dgv_xemay_CellClick;
+            // 
+            // btn_them
+            // 
+            btn_them.Location = new Point(653, 25);
+            btn_them.Name = "btn_them";
+            btn_them.Size = new Size(205, 64);
+            btn_them.TabIndex = 1;
+            btn_them.Text = "Thêm";
+            btn_them.UseVisualStyleBackColor = true;
+            btn_them.Click += btn_them_Click;
+            // 
+            // btn_sua
+            // 
+            btn_sua.Location = new Point(653, 104);
+            btn_sua.Name = "btn_sua";
+            btn_sua.Size = new Size(205, 64);
+            btn_sua.TabIndex = 2;
+            btn_sua.Text = "Sửa";
+            btn_sua.UseVisualStyleBackColor = true;
+            btn_sua.Click += btn_sua_Click;
+            // 
+            // btn_xoa
+            // 
+            btn_xoa.Location = new Point(653, 183);
+            btn_xoa.Name = "btn_xoa";
+            btn_xoa.Size = new Size(205, 64);
+            btn_xoa.TabIndex = 3;
+            btn_xoa.Text = "Xóa";
+            btn_xoa.UseVisualStyleBackColor = true;
+            btn_xoa.Click += btn_xoa_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(95, 90);
+            label1.Name = "label1";
+            label1.Size = new Size(25, 15);
+            label1.TabIndex = 4;
+            label1.Text = "Tên";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(95, 172);
+            label2.Name = "label2";
+            label2.Size = new Size(36, 15);
+            label2.TabIndex = 5;
+            label2.Text = "Hãng";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(95, 132);
+            label3.Name = "label3";
+            label3.Size = new Size(24, 15);
+            label3.TabIndex = 6;
+            label3.Text = "Giá";
+            // 
+            // txt_ten
+            // 
+            txt_ten.Location = new Point(139, 87);
+            txt_ten.Name = "txt_ten";
+            txt_ten.Size = new Size(252, 23);
+            txt_ten.TabIndex = 7;
+            // 
+            // txt_hang
+            // 
+            txt_hang.Location = new Point(139, 172);
+            txt_hang.Name = "txt_hang";
+            txt_hang.Size = new Size(252, 23);
+            txt_hang.TabIndex = 8;
+            // 
+            // txt_gia
+            // 
+            txt_gia.Location = new Point(139, 129);
+            txt_gia.Name = "txt_gia";
+            txt_gia.Size = new Size(252, 23);
+            txt_gia.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1067, 559);
+            Controls.Add(txt_gia);
+            Controls.Add(txt_hang);
+            Controls.Add(txt_ten);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(btn_xoa);
+            Controls.Add(btn_sua);
+            Controls.Add(btn_them);
+            Controls.Add(dgv_xemay);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dgv_xemay).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dgv_xemay;
+        private Button btn_them;
+        private Button btn_sua;
+        private Button btn_xoa;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox txt_ten;
+        private TextBox txt_hang;
+        private TextBox txt_gia;
+    }
+}
---
+++ XeMays.Present/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System.Diagnostics;
+using XeMays.BUS.ViewModel;
+using XeMays.BUS.Implement;
+using XeMays.BUS.Interface;
+using XeMays.DAL.Entities;
+using XeMays.Present.Extensions;
+
+namespace XeMays.Present
+{
+    public partial class Form1 : Form
+    {
+        List<XeMayVM> _xeMays;
+        IXeMayService _xeMayService;
+        private int _maXeMayChon;
+        public Form1()
+        {
+            InitializeComponent();
+            _xeMayService = new XeMayService();
+            _xeMays = new List<XeMayVM>();
+            LoadFormData();
+            LoadGridData();
+        }
+
+        private void LoadFormData()
+        {
+            dgv_xemay.Columns.Clear();
+            dgv_xemay.Columns.Add("clm1", "STT");
+            dgv_xemay.Columns.Add("clm2", "Ma");
+            dgv_xemay.Columns.Add("clm3", "Ten");
+            dgv_xemay.Columns.Add("clm4", "Gia");
+            dgv_xemay.Columns.Add("clm5", "Hang");
+        }
+
+        private void LoadGridData()
+        {
+            dgv_xemay.Rows.Clear();
+            var xeMays = _xeMayService.GetList();
+
+            Debug.WriteLine($"Số lượng xe máy: {xeMays.Count}");
+
+            foreach (var xeMay in xeMays)
+            {
+                dgv_xemay.Rows.Add(
+                    (xeMays.IndexOf(xeMay) + 1),
+                    xeMay.Id,
+                    xeMay.TenXe,
+                    xeMay.Gia,
+                    xeMay.IdHang
+                );
+            }
+        }
+
+        private int TaoIdXeMay()
+        {
+            var xeMays = _xeMayService.GetList();
+            int maxId = xeMays.Count > 0 ? xeMays.Max(xe => xe.Id) : 0;
+            return maxId + 1;
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_them_Click(object sender, EventArgs e)
+        {
+            if (MessageBoxExtension.Confirm("thêm"))
+            {
+                bool isGiaValid = double.TryParse(txt_gia.Text, out double gia);
+                bool isIdHangValid = int.TryParse(txt_hang.Text, out int idHang);
+                if (string.IsNullOrEmpty(txt_ten.Text) || !isGiaValid || gia <= 0 || !isIdHangValid)
+                {
+                    MessageBoxExtension.Notification("THÊM", "Tên không được để trống và giá cũng như mã hãng phải là số dương hợp lệ.");
+                    return;
+                }
+
+                var xeMayCreate = new XeMayCreateVM
+                {
+                    Id = TaoIdXeMay(),
+                    TenXe = txt_ten.Text,
+                    IdHang = idHang,
+                    Gia = gia
+                };
+                var result = _xeMayService.Create(xeMayCreate);
+
+                MessageBoxExtension.Notification("THÊM", result);
+                LoadGridData();
+            }
+
+        }
+
+        private void btn_sua_Click(object sender, EventArgs e)
+        {
+            if (dgv_xemay.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Vui lòng chọn một sản phẩm từ danh sách để chỉnh sửa.", "CHỈNH SỬA", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            int selectedId = Convert.ToInt32(dgv_xemay.SelectedRows[0].Cells["clm2"].Value);
+            bool isGiaValid = double.TryParse(txt_gia.Text, out double gia);
+            bool isIdHangValid = int.TryParse(txt_hang.Text, out int idHang);
+
+            if (!isGiaValid || gia <= 0 || !isIdHangValid || idHang <= 0)
+            {
+                MessageBox.Show("Giá và mã hãng phải là số dương hợp lệ.", "CHỈNH SỬA", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            var xeMayUpdateVM = new XeMayUpdateVM
+            {
+                Id = selectedId,
+                TenXe = txt_ten.Text,
+                IdHang = idHang,
+                Gia = gia
+            };
+            var result = _xeMayService.Update(xeMayUpdateVM);
+            string msg = result ? "Chỉnh sửa thành công" : "Chỉnh sửa thất bại";
+            MessageBox.Show(msg, "CHỈNH SỬA", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            LoadGridData();
+        }
+
+        private void btn_xoa_Click(object sender, EventArgs e)
+        {
+            if (dgv_xemay.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Vui lòng chọn một sản phẩm từ danh sách để xóa.", "XÓA", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            int selectedId = Convert.ToInt32(dgv_xemay.SelectedRows[0].Cells["clm2"].Value);
+
+            var confirmResult = MessageBox.Show("Bạn có chắc chắn muốn xóa sản phẩm này không?", "Xác nhận xóa", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (confirmResult == DialogResult.Yes)
+            {
+                var result = _xeMayService.Delete(selectedId);
+                string msg = result ? "Xóa thành công" : "Xóa thất bại";
+                MessageBox.Show(msg, "XÓA", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                LoadGridData();
+            }
+        }
+
+        private void dgv_xemay_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var index = e.RowIndex;
+
+            if (index < 0 || index >= _xeMays.Count)
+            {
+                _maXeMayChon = 0;
+                return;
+            }
+            var xeMayChon = _xeMays.ElementAt(index);
+
+            _maXeMayChon = xeMayChon.Id;
+            txt_ten.Text = xeMayChon.TenXe;
+            txt_hang.Text = xeMayChon.IdHang.ToString();
+            txt_gia.Text = xeMayChon.Gia.ToString();
+        }
+    }
+}
